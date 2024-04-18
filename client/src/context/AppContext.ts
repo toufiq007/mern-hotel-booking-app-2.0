cb9923d37bf86baa8c070fb008862926653f3f19
toufiq007 <t.limon2342@gmail.com>
2024-04-18 13:36:23 +0600

    facing problem to create app-context for handling toast state in frontend

---
+++ client/src/context/AppContext.ts
@@ -0,0 +1,20 @@
+import React, { useContext, useState } from "react";
+
+type ToastMessage = {
+  message: string;
+  type: "SUCCESS" | "ERROR";
+};
+
+type AppContext = {
+  showToast: (toastMessage: ToastMessage) => void;
+};
+
+const AppContext = React.createContext<AppContext | undefined>(undefined);
+
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <AppContext.provider>{children}</AppContext.provider>;
+};
